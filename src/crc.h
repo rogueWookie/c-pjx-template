64484fc12de64f0822bf709181f93629ed7aa2c8
rogueWookie <119710381+rogueWookie@users.noreply.github.com>
2022-12-02 19:08:58 -0700

    initial commit, helping someone with 2d allocation

--- src/crc.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CALC_H__
-#define __CALC_H__
-
-#include <stdint.h>
-
-uint32_t crc_crc(uint8_t* bytes, uint8_t bytessize);
-uint32_t crc_chk(uint8_t* bytes, uint8_t bytessize);
-
-#endif /*__CALC_H__*/
